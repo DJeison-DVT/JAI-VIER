d6c6c9045b65e4f90819cde85deaf3acecbf9a02
Aaron Hernandez <aypierre223@gmail.com>
2025-04-28 11:33:27 -0600

    OracleDBConfig

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/OracleConfiguration.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/OracleConfiguration.java
@@ -1,6 +1,5 @@
 package com.springboot.MyTodoList.config;
 
-
 import oracle.jdbc.pool.OracleDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,42 +8,86 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
-
 import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.SQLException;
-///*
-//    This class grabs the appropriate values for OracleDataSource,
-//    The method that uses env, grabs it from the environment variables set
-//    in the docker container. The method that uses dbSettings is for local testing
-//    @author: peter.song@oracle.com
-// */
-//
-//
+import java.util.Properties;
+
 @Configuration
 public class OracleConfiguration {
-    Logger logger = LoggerFactory.getLogger(DbSettings.class);
-    @Autowired
-    private DbSettings dbSettings;
+    Logger logger = LoggerFactory.getLogger(OracleConfiguration.class);
+    
     @Autowired
     private Environment env;
+    
     @Bean
-    public DataSource dataSource() throws SQLException{
-        OracleDataSource ds = new OracleDataSource();
-        ds.setDriverType(env.getProperty("driver_class_name"));
-        logger.info("Using Driver " + env.getProperty("driver_class_name"));
-        ds.setURL(env.getProperty("db_url"));
-        logger.info("Using URL: " + env.getProperty("db_url"));
-        ds.setUser(env.getProperty("db_user"));
-        logger.info("Using Username " + env.getProperty("db_user"));
-        ds.setPassword(env.getProperty("dbpassword"));
-//        For local testing
-//        ds.setDriverType(dbSettings.getDriver_class_name());
-//        logger.info("Using Driver " + dbSettings.getDriver_class_name());
-//        ds.setURL(dbSettings.getUrl());
-//        logger.info("Using URL: " + dbSettings.getUrl());
-//        ds.setUser(dbSettings.getUsername());
-//        logger.info("Using Username: " + dbSettings.getUsername());
-//        ds.setPassword(dbSettings.getPassword());
-        return ds;
+    public DataSource dataSource() throws SQLException {
+        try {
+            String walletLocation = env.getProperty("DB_WALLET_LOCATION");
+            System.setProperty("oracle.net.tns_admin", walletLocation);
+            logger.info("Set TNS_ADMIN to: " + walletLocation);
+            
+            OracleDataSource ds = new OracleDataSource();
+            
+            // Configuración usando variables de entorno
+            ds.setDriverType(env.getProperty("DB_DRIVER_CLASS_NAME"));
+            logger.info("Using Driver: " + env.getProperty("DB_DRIVER_CLASS_NAME"));
+            
+            ds.setURL(env.getProperty("DB_URL"));
+            logger.info("Using URL: " + env.getProperty("DB_URL"));
+            
+            ds.setUser(env.getProperty("DB_USER"));
+            logger.info("Using Username: " + env.getProperty("DB_USER"));
+            
+            ds.setPassword(env.getProperty("DB_PASSWORD"));
+            
+            // Configurar propiedades adicionales
+            Properties props = new Properties();
+            props.setProperty("oracle.net.tns_admin", walletLocation);
+            props.setProperty("oracle.net.wallet_location", 
+                             "(SOURCE=(METHOD=file)(METHOD_DATA=(DIRECTORY=" + walletLocation + ")))");
+            props.setProperty("oracle.net.ssl_server_dn_match", "true");
+            ds.setConnectionProperties(props);
+            
+            // Prueba de conexión para verificar
+            try (Connection conn = ds.getConnection()) {
+                logger.info("¡CONEXIÓN A ORACLE EXITOSA!");
+            }
+            
+            return ds;
+        } catch (SQLException e) {
+            logger.error("Error configurando Oracle DataSource: " + e.getMessage(), e);
+            
+            // Si falla, intentamos un enfoque alternativo
+            try {
+                logger.info("Trying alternate connection approach");
+                
+                OracleDataSource ds = new OracleDataSource();
+                
+                // Usar conexión directa
+                ds.setDriverType("thin");
+                ds.setServerName("adb.mx-queretaro-1.oraclecloud.com");
+                ds.setPortNumber(1522);
+                ds.setServiceName("gbd25337936632d_ixtbijmvb4aj7f1b_high.adb.oraclecloud.com");
+                ds.setUser(env.getProperty("DB_USER"));
+                ds.setPassword(env.getProperty("DB_PASSWORD"));
+                
+                // Configuración de seguridad
+                Properties props = new Properties();
+                String walletLocation = env.getProperty("DB_WALLET_LOCATION");
+                props.setProperty("oracle.net.tns_admin", walletLocation);
+                props.setProperty("oracle.net.wallet_location", 
+                                 "(SOURCE=(METHOD=file)(METHOD_DATA=(DIRECTORY=" + walletLocation + ")))");
+                props.setProperty("oracle.net.ssl_server_dn_match", "true");
+                
+                ds.setConnectionProperties(props);
+                logger.info("Configured alternate connection");
+                
+                return ds;
+            } catch (SQLException ex) {
+                logger.error("Error in alternate method: " + ex.getMessage(), ex);
+                throw ex;
+            }
+        }
     }
 }
